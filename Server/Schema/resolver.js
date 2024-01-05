2870c298d82a9f58279b8956e775df9ff3553ca6
AleSolis24 <aletitan24@icloud.com>
2024-01-04 21:22:25 -0500

    fix stuff

--- Server/Schema/resolver.js
+++ Server/Schema/resolver.js
@@ -33,14 +33,4 @@ const resolver = {
     },
   };
 
-  //const express = require('express');
-// server.js
-// const PORT = process.env.PORT || 3000;
-// const app = express();
-
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static('public'));
-
 module.exports = resolver;
