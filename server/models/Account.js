4a0b1aad79ba584bc2ae6fa796aececd84892483
auom.eisiad <daisiemouaa@gmail.com>
2024-01-04 21:30:07 -0500

    fixed code to hash the password corrently

--- server/models/Account.js
+++ server/models/Account.js
@@ -13,19 +13,26 @@ const AccountSchema = new Schema ({
     type: String,
     required: true,
     trim: true,
+    midlength: 8,
+    maxlength: 8,
   },
 });
 
 // Validate the password: hash it if it's modified or new
 AccountSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-      return next();
-    }
+  if (this.isNew || this.isModified('password')) {
 
     // Hash the password with bcrypt and 8 salt rounds
     this.password = await bcrypt.hash(this.password, 8);
-    next();
+  }
+
+  next();
 });
+
+// Compare the password with the hashed password in the database
+AccountSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const Account = model('Account', AccountSchema);
 
