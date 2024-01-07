1d83dde5ba48578e84e11182ce0fd1cdbb2c4cce
AleSolis24 <aletitan24@icloud.com>
2024-01-06 22:03:31 -0500

    Finshed resolver and added stuff in Score.js

--- Server/Schema/resolver.js
+++ Server/Schema/resolver.js
@@ -1,36 +1,55 @@
 const { Account } = require("../models/Account");
 
 const resolver = {
-    Query: {
-      users: async () => {
-        return Account.find(); 
-      },
-      user: async (parent, { userId }) => {
-        return Account.find({_id: userId});
+  Query: {
+    users: async () => {
+      return Account.find(); 
+    },
+    user: async (parent, { userId }) => {
+      return Account.find({ _id: userId });
+    },
+  },
+  Mutation: { 
+    addUser: async (parent, { username, password }) => {
+      try {
+        const newUser = await Account.create({ username, password });
+        return newUser;
+      } catch (error) {
+        throw new Error("Can not create user, please try again");
+      }
+    },
+    
+    updateUser: async (parent, { id, username }) => {
+      try {
+        const updatedUser = await Account.findByIdAndUpdate(
+          id,
+          { username },
+          { new: true }
+        );
+        return updatedUser;
+      } catch (error) {
+        throw new Error("Can not update user, please try again");
       }
     },
 
-    Mutation: { 
-        addUser: async(parent, { newUserInput }) => {
-        const newUser = await Account.create(newUserInput);
-        return newUser; 
-    },
-    userLogin: async (parent, { username, password }) =>
-    {
+    userLogin: async (parent, { username, password }) => {
+      try {
         const userInfo = await Account.findOne({ username });
-        if(!userInfo) {
-            alert('Incorrect user info');
+
+        if (!userInfo) {
+          throw new Error("Incorrect user info");
         }
 
-        const pw = await userInfo.correctPassword(password);
-        if(!pw) { 
-            alert('Incorrect password');
+        const isCorrectPassword = await userInfo.correctPassword(password);
+        if (!isCorrectPassword) {
+          throw new Error("Incorrect password");
         }
-    }, 
-
-
-
+        return { message: "Login successful", token: "yourAuthToken" };
+      } catch (error) {
+        throw new Error("Login failed");
+      }
     },
-  };
+  },
+};
 
 module.exports = resolver;
