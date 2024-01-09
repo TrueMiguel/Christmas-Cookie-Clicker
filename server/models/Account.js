2671dee535f8e2040768806e9e4cf9e6d83b6c22
Miguel Maldonado <miguel.maldonado5802@gmail.com>
2024-01-08 22:19:02 -0500

    adding server file back in

---
+++ server/models/Account.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// Schema for account model
+const AccountSchema = new Schema ({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+AccountSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+      return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 8);
+    next();
+});
+
+const Account = model('Account', AccountSchema);
+
+module.exports = Account;
---
+++ server/models/Score.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+// Schema for score model
+const ScoreSchema = new Schema({
+  score: {
+    type: Number,
+    required: true,
+    default: 0,
+  }
+});
+const Score = model('Score', ScoreSchema);
+
+async function updateUserScore(userId, clicks) {
+  try {
+    const userScore = await Score.findOne({ userId });
+
+   
+    if (userScore) {
+      
+      userScore.score += clicks;
+      await userScore.save();
+      return userScore.score;
+    } else {
+      const newUserScore = new Score({ userId, score: clicks });
+      await newUserScore.save();
+
+      
+      return clicks;
+    }
+  } catch (error) {
+    console.error("Error updating user score:", error);
+    throw new Error("Failed to update user score");
+  }
+};
+
+module.exports = Score;
