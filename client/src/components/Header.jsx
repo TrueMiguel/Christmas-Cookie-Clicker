49371e00cd18985a1d953c5e448755bbdd89314b
BenPalac1 <ben.palacpac@gmail.com>
2024-01-09 20:57:14 -0500

    got header and footer to render, standby for styling!

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,12 @@
 // may need to import useFunctions from react-router-dom if need be, and add react-router-dom to package.json 
+import React from 'react';
+
 
 const Header = () => {
     return (
         <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
             <div className="container flex-row justify-space-between-lg justify-center align-center">
-                <h1 className="m-0">Christmas Cookie Clicker Tycoon</h1>
+                <h1 className="m-0">Get Your Jingle On!</h1>
             </div>
         </header>
     );
